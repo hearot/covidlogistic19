811960d6ae1c637c297a47b81dcd65f61dca1032
Hearot <gabriel@hearot.it>
2020-03-15 17:58:23 +0100

    plot: add logarithmic scale & bar graph

--- bot.py
+++ bot.py
@@ -1,21 +1,21 @@
 # Copyright (C) Hearot - All Rights Reserved
 # Written by Gabriel Hearot <gabriel@hearot.it>.
+# See LICENSE.
 
 """Send charts about italian covid19
 expansion via Telegram using a bot."""
 
 import argparse
 import datetime
-import glob
 import html
 import schedule
 import telegram
 import time
 
 try:
-    from covid import draw_plots #noqa
+    from covid import draw_plots, extensions, files, general_excluded_plots #noqa
 except (ImportError, ModuleNotFoundError):
-    from .covid import draw_plots #noqa
+    from .covid import draw_plots, extensions, files, general_excluded_plots #noqa
 
 try:
     try:
@@ -44,7 +44,10 @@ def main():
         send_charts()
     
     while True:
-        schedule.run_pending()
+        try:
+            schedule.run_pending()
+        except Exception:
+            pass
         time.sleep(60)
 
 
@@ -58,23 +61,46 @@ def send_charts():
     
     ids.clear()
     
-    for filename in sorted(glob.glob("./*.png")):
-        with open(filename, 'rb') as file:
-            parameter = filename.replace(".png", "")[2:]
+    for filename in files:
+        photos = []
+        
+        for extension in extensions:
+            if not filename == 'general_plot' or extension not in general_excluded_plots:
+                try:
+                    with open(filename + extension, 'rb') as input_photo:
+                        photos.append(
+                            telegram.InputMediaPhoto(caption=extensions[extension],
+                                                    media=input_photo))
+                except Exception:
+                    pass
+        
+        media_ids = [message.message_id for message in 
+                     bot.send_media_group(chat_id=conf.chat,
+                                          media=photos,
+                                          disable_notification=True,
+                                          timeout=300)]
             
-            if parameter == "general_plot":
-                ids.append(
-                    bot.send_photo(chat_id=conf.chat,
-                                photo=file, 
-                                caption="📈 Grafico generale").message_id)
-            else:
-                ids.append(
-                    bot.send_photo(chat_id=conf.chat,
-                                photo=file, 
-                                caption=("📈 " + parameter.replace("_", " ").title() +
-                                         " (" + f"{data[-1][parameter]:,d}".replace(',', '.') + ", Δ=" +
-                                         f"{data[-1][parameter]-data[-2][parameter]:,d}".replace(',', '.') +
-                                         ")")).message_id)
+        if filename == "general_plot":
+            ids.append(
+                bot.send_message(chat_id=conf.chat,
+                                 disable_notification=True,
+                                 text="📈 <b>Grafico generale</b>",
+                                 parse_mode='HTML',
+                                 reply_to_message_id=media_ids[0],
+                                 timeout=300).message_id)
+        else:
+            ids.append(
+                bot.send_message(chat_id=conf.chat,
+                                 disable_notification=True,
+                                 text=("📈 <b>" + filename.replace("_", " ").title() +
+                                        "</b> (" + f"{data[-1][filename]:,d}".replace(',', '.') + ", Δ=" +
+                                        f"{data[-1][filename]-data[-2][filename]:,d}".replace(',', '.') +
+                                        ")"),
+                                 parse_mode='HTML',
+                                 reply_to_message_id=media_ids[0],
+                                 timeout=300).message_id)
+            
+        ids += media_ids
 
 
 if __name__ == '__main__':
--- covid.py
+++ covid.py
@@ -1,4 +1,6 @@
-# See LICENSE
+# Copyright (C) Hearot - All Rights Reserved
+# Written by Gabriel Hearot <gabriel@hearot.it>.
+# See LICENSE.
 
 import json
 from matplotlib import pyplot
@@ -9,23 +11,22 @@ colours = ('b', 'g', 'r', 'c', 'm', 'y',
            'k', '#4F7942', '#00FFFF', '#FF2400')
 data_url = ("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/"
             "dati-json/dpc-covid19-ita-andamento-nazionale.json")
+extensions = {'.png': "📈 Scala lineare", '_log.png': "📈 Scala logaritmica",
+              '_bars.png': "📊 Grafico a barre"}
+general_excluded_plots = ('_bars.png',)
 general_plot_parameters = ('deceduti', 'dimessi_guariti',
                            'isolamento_domiciliare', 'ricoverati_con_sintomi',
                            'tamponi', 'terapia_intensiva',
                            'totale_attualmente_positivi', 'totale_casi',
                            'totale_ospedalizzati')
-parameters = ('deceduti', 'dimessi_guariti',
-              'isolamento_domiciliare',
-              'ricoverati_con_sintomi', 'tamponi',
-              'terapia_intensiva', 'totale_attualmente_positivi',
-              'totale_casi', 'totale_ospedalizzati',
-              'nuovi_attualmente_positivi')
+parameters = general_plot_parameters + ('nuovi_attualmente_positivi',)
+files = sorted(parameters + ('general_plot',))
 
 def draw_general_plot(data):
     global general_plot_parameters
     
     pyplot.figure(1)
-    
+        
     for k, parameter in enumerate(general_plot_parameters):
         pyplot.plot(*zip(*((i['data'].split()[0][-2:],
                         i[parameter]) for i in data)), color=colours[k],
@@ -36,21 +37,33 @@ def draw_general_plot(data):
     pyplot.ylabel('numero di persone')
     pyplot.grid(True)
     pyplot.savefig("general_plot.png")
+    pyplot.yscale("log")
+    pyplot.savefig("general_plot_log.png")
 
 
 def draw_particular_plot(data, parameter: str, code: int):
     pyplot.figure(code+2)
 
-    pyplot.plot(*zip(*((i['data'].split()[0][-2:],
-                        i[parameter]) for i in data)),
+    dates, data = tuple(zip(*((i['data'].split()[0][-2:],
+                        i[parameter]) for i in data)))
+    pyplot.plot(dates, data,
                 color=colours[code],
                 label=parameter.replace('_', ' ').title())
     pyplot.legend(loc="upper left")
-    pyplot.xticks(rotation=90)
     pyplot.xlabel('giorno')
     pyplot.ylabel('numero di persone')
     pyplot.grid(True)
     pyplot.savefig(f"{parameter}.png")
+    pyplot.yscale("log")
+    pyplot.savefig(f"{parameter}_log.png")
+    pyplot.cla()
+    pyplot.bar(dates, data,
+               color=colours[code],
+               label=parameter.replace('_', ' ').title())
+    pyplot.legend(loc="upper left")
+    pyplot.xlabel('giorno')
+    pyplot.ylabel('numero di persone')
+    pyplot.savefig(f"{parameter}_bars.png")
     
 
 def draw_plots() -> List[Dict[str, Any]]:
